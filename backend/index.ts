5dd020fbd4e0a306ba297d390b5a25f6e5bae4d0
Kei <tassi.yuzukko@gmail.com>
2022-06-17 18:38:24 +0900

    vuex の追加

--- backend/index.ts
+++ backend/index.ts
@@ -1,10 +1,10 @@
-const express = require('express')
-const bodyParser = require('body-parser')
+import express from 'express';
+import bodyParser from 'body-parser';
 // corsポリシーに抵触するため、その対策としてcorsを利用する
-const cors = require('cors')
+import cors from 'cors';
 
 // データベースを使用
-const sqlite3 = require("sqlite3");
+import sqlite3 from "sqlite3";
 
 const app = express()
 app.use(bodyParser.json())
@@ -13,31 +13,31 @@ app.use(cors())
 const port = 3333;
 
 app.post('/test', function (req, res) {
-  console.log("/test に来た");
-  return res.send({
-    message: req.body.text
-  });
+    console.log("/test に来た");
+    return res.send({
+        message: req.body.text
+    });
 })
 
 app.post('/add', function (req, res) {
-  console.log(`/add に来た text:${req.body.text} text:${req.body.time}`);
+    console.log(`/add に来た text:${req.body.text} text:${req.body.time}`);
 
-  const db = new sqlite3.Database("./test.db");
-  db.serialize(() => {
-    db.run("create table if not exists members(name,time)");
-    db.run("insert into members(name,time) values(?,?)", req.body.text, req.body.time);
-    db.each("select * from members", (err, row) => {
-      console.log(`${row.name} ${row.time}`);
+    const db = new sqlite3.Database("./test.db");
+    db.serialize(() => {
+        db.run("create table if not exists members(name,time)");
+        db.run("insert into members(name,time) values(?,?)", req.body.text, req.body.time);
+        db.each("select * from members", (err, row) => {
+            console.log(`${row.name} ${row.time}`);
+        });
+        db.get("select count(*) from members", (err, count) => {
+            console.log(count["count(*)"]);
+        })
     });
-    db.get("select count(*) from members", (err, count) => {
-      console.log(count["count(*)"]);
-    })
-  });
-  db.close();
+    db.close();
 
-  return res.send({
-    message: "OK"
-  });
+    return res.send({
+        message: "OK"
+    });
 })
 
 app.listen(process.env.PORT || port, () => console.log('Example app listening http://localhost:' + port))
